f3fa84246595274028b76b7a05a2c1b4fced7c8f
Rida Zhang <nowthursday@gmail.com>
2019-05-13 07:16:33 +0900

    added dropdown d

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,10 +1,16 @@
-import { Directive } from '@angular/core';
+import { Directive, HostBinding, Input, HostListener } from '@angular/core';
+import { element, elementClassProp } from '@angular/core/src/render3';
 
 @Directive({
   selector: '[appDropdown]'
 })
 export class DropdownDirective {
-
+  @Input() appDropdown: string;
+  @HostBinding('class') elementClass: string;
   constructor() { }
+
+  @HostListener('click') mouseClicked(eventData: Event) {
+    this.elementClass = this.elementClass ? '' : this.appDropdown;
+  }
 
 }
